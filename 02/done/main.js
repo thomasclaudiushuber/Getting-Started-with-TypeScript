61302c5557df7cf3107f5c4c628dddc3311fbc45
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2016-12-23 15:53:22 +0100

    Made some changes

--- 02/done/main.js
+++ 02/done/main.js
@@ -1,9 +1,11 @@
-function printFirstNames(friends)
-{
-    for (let friend of friends) 
-    {
+function printFirstNames(friends) {
+    for (var _i = 0, friends_1 = friends; _i < friends_1.length; _i++) {
+        var friend = friends_1[_i];
         console.log(friend.firstName);
     }
 }
-
-printFirstNames('x');
+printFirstNames([
+    { firstName: "Thomas" },
+    { firstName: "Julia" },
+    { firstName: "Anna" }
+]);
---
+++ 02/done/main.ts
@@ -0,0 +1,16 @@
+interface Friend
+{
+    firstName:string;
+}
+function printFirstNames(friends:Friend[])
+{
+    for (let friend of friends) 
+    {
+        console.log(friend.firstName);
+    }
+}
+printFirstNames([
+    {firstName:"Thomas"},
+    {firstName:"Julia"},
+    {firstName:"Anna"}
+]);
