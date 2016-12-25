711963f6477093ed015ef971e4e2e92b2e4fb0fa
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2016-12-26 00:04:22 +0100

    Added simple number sample

---
+++ 04/02_number/main.js
@@ -0,0 +1,8 @@
+var dec = 27;
+console.log(dec);
+var hex = 0x001b;
+console.log(hex);
+var binary = 27;
+console.log(binary);
+var octal = 27;
+console.log(octal);
---
+++ 04/02_number/main.ts
@@ -0,0 +1,11 @@
+let dec:number = 27;
+console.log(dec);
+
+let hex:number = 0x001b;
+console.log(hex);
+
+let binary:number = 0b11011;
+console.log(binary);
+
+let octal:number= 0o0033;
+console.log(octal);
