997bce47a9293723d22eb1b3a88ac0225648849e
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2016-12-25 23:39:36 +0100

    Added boolean samples

---
+++ 04/boolean/main.ts
@@ -0,0 +1,19 @@
+// Implicitly typed variable
+let isVisible1 = true;
+isVisible1 = "hidden"; // ERROR: string not assignable to type boolean
+
+// Declaration and assignment split: Without type
+let isVisible2;
+isVisible2 = true;
+isVisible2 = "hidden"; // WORKS, as type cannot be infered
+ 
+// Declaration and assignment split: With type
+let isVisible3:boolean;
+isVisible3 = true;
+isVisible3 = "hidden"; // ERROR: string not assignable to type boolean
+
+
+
+
+
+
---
+++ 04/boolean/readme.txt
@@ -0,0 +1,10 @@
+1. Open folder in Visual Studio Code
+
+2. Look at the errors you get for the implicitly typed variables
+
+3. Understand that TypeScript can't infer the type if you split up
+   declaration and assigment
+
+4. Play around with the noEmitOnError-compiler option:
+   When true:  Call "tsc" and NO "main.js" is generated
+   Otherwise: Call "tsc" and "main.js" is generated
