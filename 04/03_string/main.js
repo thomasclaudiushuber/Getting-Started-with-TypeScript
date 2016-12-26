ef6b7ce09fbd41f33860c13224261cd06d30d24b
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2016-12-26 11:27:47 +0100

    Added additional string example

--- 04/03_string/main.js
+++ 04/03_string/main.js
@@ -1,11 +1,4 @@
-function onButtonClick() {
-    // Grab the two elements by id
-    var input = document.getElementById("username");
-    var span = document.getElementById("output");
-    // read from the input element
-    var name = input.value;
-    // create the message
-    var message = "Welcome " + name + ", how are you?";
-    // write the message to the span-element
-    span.innerText = message;
-}
+let firstName = "Thomas";
+let message = `Welcome ${firstName},
+how are you?`;
+console.log(message);
--- 04/03_string/main.ts
+++ 04/03_string/main.ts
@@ -1,15 +1,5 @@
-function onButtonClick() 
-{
-    // Grab the two elements by id
-    let input = document.getElementById("username") as HTMLInputElement;
-    let span = document.getElementById("output");
+let firstName:string = "Thomas";
+let message = `Welcome ${firstName },
+how are you?`;
 
-    // read from the input element
-    let name = input.value;
-
-    // create the message
-    let message = `Welcome ${name}, how are you?`;
-
-    // write the message to the span-element
-    span.innerText = message;
-}
+console.log(message);
