d058a5d0fd8f4b143ceb0b457dace4d63a1e5916
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2016-12-27 11:51:58 +0100

    Added enum sample

---
+++ 04/08_enums/main.js
@@ -0,0 +1,11 @@
+var Dock;
+(function (Dock) {
+    Dock[Dock["left"] = 0] = "left";
+    Dock[Dock["top"] = 1] = "top";
+    Dock[Dock["right"] = 2] = "right";
+    Dock[Dock["bottom"] = 3] = "bottom";
+})(Dock || (Dock = {}));
+let dock = Dock.top;
+console.log(typeof dock);
+console.log(dock);
+console.log(Dock[dock]);
---
+++ 04/08_enums/main.ts
@@ -0,0 +1,7 @@
+enum Dock { left, top,right, bottom }
+
+let dock:Dock = Dock.top;
+
+console.log(typeof dock);
+console.log(dock);
+console.log(Dock[dock]);
