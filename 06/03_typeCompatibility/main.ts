fbe048b6dca2aef078f265ecfbaef867c9c5f2d4
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-01-04 20:13:40 +0100

    Added typeCompability sample

---
+++ 06/03_typeCompatibility/main.ts
@@ -0,0 +1,11 @@
+interface Developer {
+    knowsTypeScript: boolean;
+}
+
+// Note that the Friend-class does not implement the Developer-interface
+class Friend {
+    knowsTypeScript: boolean;
+}
+
+// This works due to "structural typing" of TypeScript
+let dev:Developer = new Friend();
