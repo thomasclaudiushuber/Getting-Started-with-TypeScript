38bf44717438e313216245fbb67abb803e9ae35f
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-01-05 19:15:48 +0100

    Added generic array sample

---
+++ 07/01_genericArray/main.js
@@ -0,0 +1,4 @@
+var array = new Array();
+array.push(1);
+array.push(2);
+array.push("Hello"); // Error, string not assignable to number
---
+++ 07/01_genericArray/main.ts
@@ -0,0 +1,6 @@
+let array=new Array<number>();
+
+array.push(1);
+array.push(2);
+array.push("Hello"); // Error, string not assignable to number
+
