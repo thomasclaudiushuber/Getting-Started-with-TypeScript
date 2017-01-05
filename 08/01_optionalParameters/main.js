3184fb37f3bb0dea7d772acbe8c0ec688c395418
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-01-05 20:38:49 +0100

    Added optional parameters

---
+++ 08/01_optionalParameters/main.js
@@ -0,0 +1,9 @@
+function getFullName(firstName, lastName) {
+    if (lastName)
+        return firstName + " " + lastName;
+    else
+        return firstName;
+}
+console.log(getFullName("Thomas"));
+console.log(getFullName("Thomas", "Huber"));
+console.log(getFullName()); // Error: parameters do not match signature
---
+++ 08/01_optionalParameters/main.ts
@@ -0,0 +1,10 @@
+function getFullName(firstName: string, lastName?: string) {
+    if (lastName)
+        return `${firstName} ${lastName}`;
+    else
+        return firstName;
+}
+
+console.log(getFullName("Thomas"));
+console.log(getFullName("Thomas", "Huber"));
+console.log(getFullName()); // Error: parameters do not match signature
