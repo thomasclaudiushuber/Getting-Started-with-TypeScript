796523ac22aa730dc8a019f4e26bef0bc44ae345
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-01-05 20:38:55 +0100

    Added defaultParameters

---
+++ 08/02_defaultParameters/main.js
@@ -0,0 +1,10 @@
+function getFullName(firstName, lastName) {
+    if (firstName === void 0) { firstName = "Julia"; }
+    if (lastName)
+        return firstName + " " + lastName;
+    else
+        return firstName;
+}
+console.log(getFullName("Thomas"));
+console.log(getFullName("Thomas", "Huber"));
+console.log(getFullName()); // OK, logs "Julia"
---
+++ 08/02_defaultParameters/main.ts
@@ -0,0 +1,10 @@
+function getFullName( firstName: string = "Julia",lastName?: string) {
+    if (lastName)
+        return `${firstName} ${lastName}`;
+    else
+        return firstName;
+}
+
+console.log(getFullName("Thomas"));
+console.log(getFullName("Thomas", "Huber"));
+console.log(getFullName()); // OK, logs "Julia"
