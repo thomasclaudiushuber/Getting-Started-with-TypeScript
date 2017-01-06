7c2a946c817db9cbcd7411a60e2e9b4c2c73fd3a
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-01-06 17:54:28 +0100

    Added multiple import export sample

---
+++ 09/03_modules_MultipleExportImport/main.ts
@@ -0,0 +1,7 @@
+import { Developer,Skateboarder } from './friends.js';
+
+var dev = new Developer("Julia");
+dev.knowsTypeScript = true;
+
+var boarder = new Skateboarder("Thomas");
+boarder.makeKickflip();
