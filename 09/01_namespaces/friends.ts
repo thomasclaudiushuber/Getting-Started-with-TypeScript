fd8a5d54bd51aad627c35c9919cfe9f52b01587e
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-01-07 23:09:00 +0100

    removed namespace sample

--- 09/01_namespaces/friends.ts
+++
@@ -1,8 +0,0 @@
-namespace Friends {
-    export class Friend {
-        constructor(public firstName: string) { }
-    }
-    export class Developer extends Friend {
-        knowsTypeScript: boolean;
-    }
-}
--- 09/01_namespaces/main.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="./friends.ts" />
-var f: Friends.Friend = new Friends.Developer("Thomas");
-
-console.log(f.firstName);
