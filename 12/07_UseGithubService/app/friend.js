b45f961b929cfbb733c82f1b83d839214e464d4e
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-01-28 22:09:33 +0100

    Removed js-files from angular samples

--- 12/07_UseGithubService/app/friend.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.FRIENDLIST = [{ name: "Thomas", github: "thomasclaudiushuber" },
-    { name: "Microsoft", github: "microsoft" },
-    { name: "Angular Team", github: "angular" }];
-//# sourceMappingURL=friend.js.map
