3ba742c8ad76b458d45806a7323ba8e3ad5ada9a
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-02-26 11:35:15 +0100

    Adjust parameter with type

--- 07/06_multipleTypeParameters/main.ts
+++ 07/06_multipleTypeParameters/main.ts
@@ -1,9 +1,8 @@
 class Dictionary<TKey, TValue> {
   private _items = new Array<[TKey, TValue]>();
-  getItem(TKey): TValue {
-    let foundItems  = this._items.filter(item => item[0] == TKey);
-    if(foundItems.length!=1)
-    {
+  getItem(key: TKey): TValue {
+    let foundItems = this._items.filter(item => item[0] == key);
+    if (foundItems.length != 1) {
       throw new Error("Item with key does not exist");
     }
     return foundItems[0][1]; // Grab the tuple ([0]) and then the value ([1])
@@ -18,10 +17,10 @@ class Dictionary<TKey, TValue> {
   }
 }
 
-var dict = new Dictionary<number,string>();
-dict.add(1,"Thomas");
-dict.add(2,"Julia");
-dict.add(3,"Anna");
+var dict = new Dictionary<number, string>();
+dict.add(1, "Thomas");
+dict.add(2, "Julia");
+dict.add(3, "Anna");
 
-let firstName:string= dict.getItem(2);
+let firstName: string = dict.getItem(2);
 console.log(firstName);
